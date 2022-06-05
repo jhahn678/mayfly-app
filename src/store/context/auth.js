3f88cad3f7241d23890cf504131f7b88d20efa6b
jhahn678 <jhahn678@yahoo.com>
2022-06-05 15:51:25 -0400

    Added auth context

---
+++ src/store/context/auth.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from 'react'
+import * as SecureStore from 'expo-secure-store'
+
+const initialState = {
+    isSignedIn: false,
+    user: {},
+    token: '',
+    signIn: async (user, token) => {},
+    signOut: async () => {}
+}
+
+const AuthContext = createContext(initialState)
+
+export const AuthProvider = ({ children }) => {
+
+    const [isSignedIn, setIsSignedIn] = useState(false)
+    const [user, setUser] = useState({})
+    const [token, setToken] = useState('')
+
+    const signIn = async (user, token) => {
+        console.log(user)
+        await SecureStore.setItemAsync('ACCESS_TOKEN', token)
+        setIsSignedIn(true)
+        setUser()
+        setToken(token)
+    }
+
+    const signOut = async () => {
+        await SecureStore.deleteItemAsync('ACCESS_TOKEN')
+        setIsSignedIn(false)
+        setUser({})
+        setToken('')
+    }
+
+    return(
+        <AuthContext.Provider value={{ 
+            isSignedIn, user, token,
+            signIn, signOut
+        }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuthContext = () => useContext(AuthContext)
