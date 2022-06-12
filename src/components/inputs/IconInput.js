3eb01cf03299a57132193c41e642fdd8f75e8b6e
jhahn678 <jhahn678@yahoo.com>
2022-06-12 16:46:21 -0400

    Added prop for error

--- src/components/inputs/IconInput.js
+++ src/components/inputs/IconInput.js
@@ -1,10 +1,11 @@
-import { StyleSheet, View, TextInput } from 'react-native'
+import { StyleSheet, View, TextInput, Text } from 'react-native'
 import React from 'react'
 
-export default function IconInput({ Icon, containerStyle, value, setValue, label }) {
+export default function IconInput({ Icon, containerStyle, value, setValue, label, isError, error, isPassword }) {
   return (
-    <View style={{...styles.inputContainer, ...containerStyle}}>
-        <View style={styles.inputIcon}>
+    <View style={containerStyle}>
+    <View style={{...styles.inputContainer, borderColor: isError && '#df2c0c' }}>
+        <View style={{...styles.inputIcon, borderRightColor: isError && '#df2c0c'}}>
             {Icon}
         </View>
         <TextInput placeholder={label}
@@ -12,7 +13,11 @@ export default function IconInput({ Icon, containerStyle, value, setValue, label
             value={value} 
             onChangeText={setValue} 
             style={styles.inputField}
+            secureTextEntry={isPassword}
+            selectionColor='#0A3542'
         />
+    </View>
+    { error && <Text style={styles.errorMessage}>{error}</Text>}
     </View>
   )
 }
@@ -25,6 +30,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 8,
         backgroundColor: 'rgba(255, 254, 243, .3)',
+        marginBottom: 6
       },
       inputIcon: {
         marginRight: 4,
@@ -38,5 +44,10 @@ const styles = StyleSheet.create({
         fontSize: 20,
         width: '80%',
         padding: 6
-      }
+      },
+      errorMessage: {
+        fontSize: 14,
+        paddingLeft: 8,
+        color: '#df2c0c'
+      },
 })
