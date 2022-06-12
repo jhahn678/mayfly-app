84fdcc0a4776ec4ad52fbb43b5995bb66d52c8be
jhahn678 <jhahn678@yahoo.com>
2022-06-12 16:42:13 -0400

    Added global styles

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,5 +1,22 @@
 import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
-    
+    fontShadow: {
+        color: '#fffef3', 
+        textShadowColor: 'rgba(0,0,0,.3)',
+        textShadowOffset: { height: 4 },
+        textShadowRadius: 8
+    },
+    boxShadowBottom: {
+        shadowColor: 'black',
+        shadowOpacity: .3,
+        shadowOffset: { height: 4 },
+        shadowRadius: 8,
+    },
+    boxShadowTop: {
+        shadowColor: 'black',
+        shadowOpacity: .3,
+        shadowOffset: { height: -4 },
+        shadowRadius: 8,
+    }
 })
