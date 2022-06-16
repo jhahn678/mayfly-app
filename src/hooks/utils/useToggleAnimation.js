959c0bceef8abcd989478919af4fae9a58e0e97d
jhahn678 <jhahn678@yahoo.com>
2022-06-16 19:06:32 -0400

    Added a hook form RN animated

---
+++ src/hooks/utils/useToggleAnimation.js
@@ -0,0 +1,32 @@
+import { useState, useRef, useEffect } from 'react'
+import { Animated } from 'react-native'
+
+export const useToggleAnimation = ({ initialValue=1, animatedValue=0, duration, durationOn=150, durationOff=150 }) => {
+
+    const ref = useRef(new Animated.Value(initialValue)).current
+    const [toggledOn, setToggledOn] = useState(false)
+
+    useEffect(() => {
+        if(toggledOn) {
+        //Toggle off
+            Animated.timing(ref, { 
+                toValue: initialValue, 
+                duration: duration ? duration : durationOff, 
+                useNativeDriver: true
+            }).start()
+        }else{
+        //Toggle on
+            Animated.timing(ref, { 
+                toValue: animatedValue, 
+                duration: duration ? duration : durationOn, 
+                useNativeDriver: true
+            }).start()
+        }
+    },[toggledOn])
+
+    return {
+        ref,
+        toggledOn,
+        setToggledOn
+    }
+}
