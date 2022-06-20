3679dcd76286dd179f070a1a2701aac18fca7d01
jhahn678 <jhahn678@yahoo.com>
2022-06-20 10:05:08 -0400

    Component for image slider

---
+++ src/components/image/FlatListImage.js
@@ -0,0 +1,35 @@
+import { StyleSheet, View, Image } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const FlatListImage = ({ image, style, clearImage }) => {
+  return (
+    <View style={{...styles.container, ...style}}>
+        <Image source={{ uri: image.uri }} resizeMode='cover' 
+           style={styles.image}
+        />
+        <Icon name='remove-circle' size={28} style={styles.remove} onPress={clearImage}/>
+    </View>
+  )
+}
+
+export default FlatListImage
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative'
+    },
+    image: {
+        height: '100%',
+        width: '100%'
+    },
+    remove: {
+        position: 'absolute',
+        top: 8,
+        right: 8,
+        color: '#fefefe',
+        backgroundColor: 'rgba(255,0,0,.6)',
+        borderRadius: 16,
+        paddingLeft: 2,
+        overflow: 'hidden',
+    }
+})
