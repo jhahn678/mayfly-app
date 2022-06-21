21bb03f70e857aab4a51776a9dc55dc70fedcb1e
jhahn678 <jhahn678@yahoo.com>
2022-06-21 12:14:44 -0400

    added disabled prop

--- src/components/buttons/CheckmarkFAB.js
+++ src/components/buttons/CheckmarkFAB.js
@@ -2,12 +2,13 @@ import { StyleSheet } from 'react-native'
 import { FAB } from '@rneui/themed'
 import Icon from 'react-native-vector-icons/Ionicons'
 
-const CheckmarkFAB = ({ size='large', style, onPress }) => {
+const CheckmarkFAB = ({ size='large', style, onPress, disabled }) => {
 
     return (
         <FAB icon={<Icon name='checkmark' size={24} color='#fefefe'/>} 
-            onPress={onPress} size={size}
-            style={{ ...styles.button, ...style}}
+            onPress={onPress} size={size} style={disabled ? {...style} : { ...styles.button, ...style}}
+            disabledStyle={{ backgroundColor: 'rgb(220,220,220)', opacity: .7 }}
+            disabled={disabled}
         />
     )
 }
