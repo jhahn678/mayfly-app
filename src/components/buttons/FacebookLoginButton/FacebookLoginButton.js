61b0a7dd5b75abad3df45f76325829b083ed950b
jhahn678 <jhahn678@yahoo.com>
2022-07-02 15:31:04 -0400

    google/facebook login implementation

--- src/components/buttons/FacebookLoginButton/FacebookLoginButton.js
+++ src/components/buttons/FacebookLoginButton/FacebookLoginButton.js
@@ -1,11 +1,47 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { useState, useEffect } from 'react'
+import * as WebBrowser from 'expo-web-browser';
+import * as Facebook from 'expo-auth-session/providers/facebook';
+import { ResponseType } from 'expo-auth-session';
 import FacebookIcon from '../../icons/FacebookIcon'
+import { useAuthContext } from '../../../store/context/auth';
 
-const FacebookLoginButton = ({ containerStyle, text, onClick, iconSize }) => {
+WebBrowser.maybeCompleteAuthSession()
+
+const FacebookLoginButton = ({ containerStyle, text, iconSize }) => {
+
+    const { signIn } = useAuthContext()
+
+    const [request, response, promptAsync] = Facebook.useAuthRequest({
+        expoClientId: process.env.FACEBOOK_APP_ID,
+        responseType: ResponseType.Token,
+        scopes: ['email', 'public_profile']
+    })
+
+    useEffect(() => {
+        if(response?.type === 'success'){
+            (async () => {
+                try{
+                    const { authentication } = response;
+                    const { data } = await axios.post('/auth/facebook', {
+                        token: authentication.accessToken
+                    })
+                    signIn(data.user, data.token)
+                }catch(err){
+                    alert('Something went wrong!')
+                }
+            })()
+        }
+    },[response])
+
+    const handleFacebookLogin = async () => {
+        promptAsync()
+    }
 
     return (
-        <TouchableOpacity style={{ ...styles.container, ...containerStyle}} onClick={onClick}>
+        <TouchableOpacity style={{ ...styles.container, ...containerStyle}} 
+            onPress={handleFacebookLogin} disabled={!request}
+        >
             <FacebookIcon containerStyle={styles.icon} size={iconSize}/>
             <Text style={styles.text}>{text}</Text>
         </TouchableOpacity>
--- src/components/buttons/GoogleLoginButton/GoogleLoginButton.js
+++ src/components/buttons/GoogleLoginButton/GoogleLoginButton.js
@@ -1,11 +1,47 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { useEffect } from 'react'
+import * as Google from 'expo-auth-session/providers/google'
+import * as WebBrowser from 'expo-web-browser'
 import GoogleIcon from '../../icons/GoogleIcon'
+import axios from '../../../utils/axios'
+import { ResponseType } from 'expo-auth-session'
+import { useAuthContext } from '../../../store/context/auth' 
 
-const GoogleLoginButton = ({ containerStyle, text, onClick, iconSize }) => {
+WebBrowser.maybeCompleteAuthSession();
+
+const GoogleLoginButton = ({ containerStyle, text, iconSize }) => {
+
+
+    const { signIn } = useAuthContext()
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        expoClientId: process.env.GOOGLE_EXPO_CLIENT_ID,
+        responseType: ResponseType.IdToken
+    })
+
+    useEffect(() => {
+        if(response?.type === 'success'){
+            const { id_token } = response.params;
+            (async () => {
+                try{
+                    const { authentication } = response;
+                    const { data } = await axios.post('/auth/google', {
+                        token: id_token
+                    })
+                    signIn(data.user, data.token)
+                }catch(err){
+                    alert('Something went wrong!')
+                }
+            })()
+        }
+    },[response])
+
+    const handleGoogleLogin = async () => {
+        promptAsync()
+    }
   
     return (
-        <TouchableOpacity style={{ ...styles.container, ...containerStyle}} onClick={onClick}>
+        <TouchableOpacity style={{ ...styles.container, ...containerStyle}} onPress={handleGoogleLogin}>
             <GoogleIcon containerStyle={styles.icon} size={iconSize}/>
             <Text style={styles.text}>{text}</Text>
         </TouchableOpacity>
