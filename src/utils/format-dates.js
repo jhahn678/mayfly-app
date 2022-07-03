0aee143de585af8b3da8226bc0294b56fb17e136
jhahn678 <jhahn678@yahoo.com>
2022-07-03 17:03:18 -0400

    Formatting different date formats from api

--- src/utils/format-dates.js
+++ src/utils/format-dates.js
@@ -1,5 +1,6 @@
 import moment from 'moment'
 
+
 export const formatDateGroupList = (isoDate) => {
     return moment(isoDate).calendar()
 }
@@ -8,10 +9,24 @@ export const formatTimeMessage = (isoDate) => {
     return moment(isoDate).format('h:mm a')
 }
 
-export const formatCreatedAt = (isoDate) => {
-    return moment(isoDate).format('M/D/YYYY [at] h:mm a')
+export const formatCreatedAt = (value) => {
+    const date = moment(value)
+    if(date){
+        return date.format('M/D/YYYY [at] h:mm a')
+    }else{
+        const num = parseInt(msDateString)
+        const isoString = new Date(num)
+        return moment(isoString).format('M/D/YYYY [at] h:mm a')
+    }
 }   
 
-export const formatProfileCreatedAt = (isoDate) => {
-    return moment(isoDate).format('M/D/YY')
+export const formatProfileCreatedAt = (value) => {
+    const date = moment(value)
+    if(date){
+        return date.format('M/D/YY')
+    }else{
+        const num = parseInt(msDateString)
+        const isoString = new Date(num).toISOString()
+        return moment(isoString).format('M/D/YY')
+    }
 }
