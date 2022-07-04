915239cb5d980f30b2809f9c9dfbfbd5f7b5f1de
jhahn678 <jhahn678@yahoo.com>
2022-07-04 00:30:42 -0400

    Added custom scalar types in backend to make dates easier here

--- src/utils/format-dates.js
+++ src/utils/format-dates.js
@@ -9,24 +9,10 @@ export const formatTimeMessage = (isoDate) => {
     return moment(isoDate).format('h:mm a')
 }
 
-export const formatCreatedAt = (value) => {
-    const date = moment(value)
-    if(date){
-        return date.format('M/D/YYYY [at] h:mm a')
-    }else{
-        const num = parseInt(msDateString)
-        const isoString = new Date(num)
-        return moment(isoString).format('M/D/YYYY [at] h:mm a')
-    }
+export const formatCreatedAt = (isoDate) => {
+    return moment(isoDate).format('M/D/YYYY [at] h:mm a')
 }   
 
-export const formatProfileCreatedAt = (value) => {
-    const date = moment(value)
-    if(date){
-        return date.format('M/D/YY')
-    }else{
-        const num = parseInt(msDateString)
-        const isoString = new Date(num).toISOString()
-        return moment(isoString).format('M/D/YY')
-    }
+export const formatProfileCreatedAt = (isoDate) => {
+    return moment(isoDate).format('M/D/YY')
 }
